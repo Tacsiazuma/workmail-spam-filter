b73a4b92f13b4e35cddcc3ba062f2ff933fd236f
Krisztian Papp <fejlesztes@letscode.hu>
2024-01-27 17:52:00 +0100

    added s3 and fixed deprecation warn

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,7 @@ import { Construct } from 'constructs';
 import * as nodejs from 'aws-cdk-lib/aws-lambda-nodejs';
 import * as lambda from 'aws-cdk-lib/aws-lambda';
 import * as iam from 'aws-cdk-lib/aws-iam';
+import * as s3 from 'aws-cdk-lib/aws-s3';
 import * as logs from 'aws-cdk-lib/aws-logs';
 
 const path = require("node:path")
@@ -11,14 +12,35 @@ export class WorkmailSpamFilterStack extends cdk.Stack {
     constructor(scope: Construct, id: string, props?: WorkmailSpamFilterStackProps) {
         super(scope, id, props);
 
+        const logGroup = new logs.LogGroup(this, 'SpamFilterLogs', {
+            retention: logs.RetentionDays.ONE_MONTH,
+        })
+        const spamBucket = new s3.Bucket(this, 'SpamBucket')
         const spamFilter = new nodejs.NodejsFunction(this, 'SpamFilter', {
             entry: path.join(__dirname, '../src/function/index.ts'),
             architecture: lambda.Architecture.ARM_64,
             functionName: 'SpamFilter',
             timeout: cdk.Duration.minutes(1),
             description: 'Lambda to handle spam messages',
-            logRetention: logs.RetentionDays.ONE_MONTH,
+            logGroup: logGroup,
+            environment: {
+                BUCKET_NAME: spamBucket.bucketName
+            }
         });
+        spamFilter.addToRolePolicy(new iam.PolicyStatement({
+            actions: ['s3:GetObject'],
+            resources: [spamBucket.bucketArn + '/*'],
+        }));
+        spamFilter.role.attachInlinePolicy(
+            new iam.Policy(this, 'GetRawMessageContent', {
+                statements: [
+                    new iam.PolicyStatement({
+                        actions: ['workmailmessageflow:GetRawMessageContent'],
+                        resources: ['*'],
+                    }),
+                ],
+            })
+        );
         spamFilter.addPermission("AllowWorkMail", {
             action: "lambda:InvokeFunction",
             principal: new iam.ServicePrincipal(`workmail.${props.env.region}.amazonaws.com`),
